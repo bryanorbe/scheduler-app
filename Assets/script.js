b9ce6ca8b6fc57c58509d6175b16db3340147b2f
Bryan Orbe <bryan.orbe@gmail.com>
2022-04-03 06:09:38 -0400

    fixed file hierarchy and added framework to script

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+//set military time
+
+//add current date
+
+//program the timeslots for each hour (x9)
+
+//add functionality to change current time to red, future time to green & past time to gray
+
